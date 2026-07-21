42dd60ff95c54a81113366c7ab4fe6617a86f4ee
jcarlosmber <jcarlosmber@gmail.com>
2026-07-20 22:01:10 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ const PORT = process.env.PORT || 3000;
 
 // Configuración de Middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Workaround para WAF: Si recibimos POST pero con X-HTTP-Method-Override,
 // cambiamos internamente el req.method para engañar a Express y al WAF.
